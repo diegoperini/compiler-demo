5d89226b3b89e03a933cc39859922ee7a7bd2224
Diego Perini <diego@dperini.com>
2018-10-24 01:51:49 +0300

    Added better type checking to llvm IR generator

--- src/llvm-demo.ts
+++ src/llvm-demo.ts
@@ -4,37 +4,38 @@ import * as llvm from "llvm-node"
 let context = new llvm.LLVMContext
 
 // Native Types
+type IntT = { t: llvm.Type, signed: boolean }
 let Int8Type = llvm.Type.getInt8Ty(context)
-function getInt8Type() : { t:llvm.Type, signed: boolean } {
+function getInt8Type() : IntT {
   return { t: Int8Type, signed: true }
 }
 let Int16Type = llvm.Type.getInt16Ty(context)
-function getInt16Type() : { t:llvm.Type, signed: boolean } {
+function getInt16Type() : IntT {
   return { t: Int16Type, signed: true }
 }
 let Int32Type = llvm.Type.getInt32Ty(context)
-function getInt32Type() : { t:llvm.Type, signed: boolean } {
+function getInt32Type() : IntT {
   return { t: Int32Type, signed: true }
 }
 let Int64Type = llvm.Type.getInt64Ty(context)
-function getInt64Type() : { t:llvm.Type, signed: boolean } {
+function getInt64Type() : IntT {
   return { t: Int64Type, signed: true }
 }
 
 let Uint8Type = llvm.Type.getInt8Ty(context)
-function getUint8Type() : { t:llvm.Type, signed: boolean } {
+function getUint8Type() : IntT {
   return { t: Uint8Type, signed: false }
 }
 let Uint16Type = llvm.Type.getInt16Ty(context)
-function getUint16Type() : { t:llvm.Type, signed: boolean } {
+function getUint16Type() : IntT {
   return { t: Uint16Type, signed: false }
 }
 let Uint32Type = llvm.Type.getInt32Ty(context)
-function getUint32Type() : { t:llvm.Type, signed: boolean } {
+function getUint32Type() : IntT {
   return { t: Uint32Type, signed: false }
 }
 let Uint64Type = llvm.Type.getInt64Ty(context)
-function getUint64Type() : { t:llvm.Type, signed: boolean } {
+function getUint64Type() : IntT {
   return { t: Uint64Type, signed: false }
 }
 
@@ -43,36 +44,41 @@ function getUint64Type() : { t:llvm.Type, signed: boolean } {
 // function getFloat16Type() : { t: llvm.Type } {
 //   return { t: Float16Type }
 // }
+type FloatT = { t: llvm.Type }
 let Float32Type = llvm.Type.getFloatTy(context)
-function getFloat32Type() : { t: llvm.Type } {
+function getFloat32Type() : FloatT {
   return { t: Float32Type }
 }
 let Float64Type = llvm.Type.getDoubleTy(context)
-function getFloat64Type() : { t: llvm.Type } {
+function getFloat64Type() : FloatT {
   return { t: Float64Type }
 }
 
+type BoolT = { t: llvm.Type, bool: boolean }
 let BoolType = llvm.Type.getInt8Ty(context)
-function getBoolType() : { t: llvm.Type, bool: boolean } {
+function getBoolType() : BoolT {
   return { t: BoolType, bool: true }
 }
 
+type VoidT = { t: llvm.Type }
 let VoidType = llvm.Type.getVoidTy(context)
-function getVoidType() : { t: llvm.Type, bool: boolean } {
-  return { t: VoidType, bool: true }
+function getVoidType() : VoidT {
+  return { t: VoidType }
 }
 
+type StringT = { t: llvm.Type, count: number }
 let StringType = llvm.Type.getInt8Ty(context)
-function getStringType(count: number) : { t: llvm.Type, count: number } {
+function getStringType(count: number) : StringT {
   return { t: llvm.ArrayType.get(StringType, count), count: count }
 }
 
 // Scopes
-function createModule(name: string) {
+function createModule(name: string) : llvm.Module {
   return new llvm.Module(name, context)
 }
 
-function createMain(llvmModule: llvm.Module) : { mainBlock: llvm.BasicBlock, mainBuilder: llvm.IRBuilder, mainReturnAlloca: llvm.AllocaInst} {
+type Main = { mainBlock: llvm.BasicBlock, mainBuilder: llvm.IRBuilder, mainReturnAlloca: llvm.AllocaInst}
+function createMain(llvmModule: llvm.Module) : Main {
   let intType = llvm.Type.getInt32Ty(context)
   let mainFuncType = llvm.FunctionType.get(intType, false)
   let mainFunc = llvmModule.getOrInsertFunction("main", mainFuncType)
@@ -103,7 +109,8 @@ function writeBitcodeToFile(llvmModule: llvm.Module, filePath: string) {
 
 function test() {
   let m = createModule("test")
-  let main = createMain(m)
+
+  createMain(m)
 
   logIR(m)
   writeBitcodeToFile(m, "./lol.bit")
